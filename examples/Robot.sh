9b7aedbe63412d4674b832991bf7efc3b2b56f56
silverdirk <silverdirk@conserv.silverdirk.com>
2005-05-15 23:38:23 +0000

    Moved the change-interpreter-to-bash kluge to its own file, to be sourced by other scripts

--- examples/Robot.sh
+++ examples/Robot.sh
@@ -1,12 +1,6 @@
 #!/bin/sh
 
-# Thank you, FreeBSD, for making this &@^%ing kluge necessary.
-# We can all tell how dedicated you are to making things work out of the box.
-# Perhaps you'd like to relocate 'sh' as well?  How about
-#  /usr/shells/bin/sh ?  or perhaps /sys/bin/sh ?
-if [ -z "$BASH" ]; then
-	exec bash $0
-fi
+source BashInterpKluge.sh
 
 die() { echo $1; exit -1; }
 
